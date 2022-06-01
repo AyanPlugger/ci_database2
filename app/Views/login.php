da416fad61736aa5c50211c58645f613d4844296
AyanPlugger <ayanpruggertattoo@gmail.com>
2022-06-01 14:59:44 -0300

    a

---
+++ app/Views/login.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-4 offset-4">
+        <h2>Login</h2>
+    </div>
+</div>
+<form action="logar" method="post">
+<div class="row">
+    <div class="col-4 offset-4">
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" class="form-control id="nome">
+    </div>
+</div>
+<div class="row">
+    <div class="col-4 offset-4">
+        <label for="senha">Senha:</label>
+        <input type="password" name="senha" class="form-control id="password">
+    </div>
+</div>
+<div class="row">
+    <div class="col-4 offset-4">
+        <button class="btn btn-primary mt-3" type="submit">Entrar</button>
+    </div>
+</div>
+</form>
